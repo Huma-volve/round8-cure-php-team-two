7199eb88b587dfb273973be546e47fd599e90c4c
Mohammed-tareq <mohtareq1999m@gmail.com>
2025-12-02 07:55:05 +0200

    Fix typo: rename 'Massage' to 'Message' in table and model names

--- database/migrations/2025_12_01_192650_create_messages_table.php
+++ database/migrations/2025_12_01_192650_create_messages_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('massages', function (Blueprint $table) {
+        Schema::create('messages', function (Blueprint $table) {
             $table->id();
             $table->foreignId('chat_id')->constrained('chats')->onDelete('cascade');
             $table->morphs('sender');
