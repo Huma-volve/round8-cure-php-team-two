54e6ab818f48012b1eddae23dff03d7e58b94809
Rana Hamdy <104355652+ranahamdy1@users.noreply.github.com>
2025-12-07 18:44:56 +0200

    edit auth seeder in db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             SpecialtySeeder::class,
             DoctorSeeder::class,
+            RoleSeeder::class,
         ]);
     }
 }
