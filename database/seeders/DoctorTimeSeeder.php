427c10456261871dcb63326430407f43d4b9a000
Rana Hamdy <104355652+ranahamdy1@users.noreply.github.com>
2025-12-29 14:10:42 +0200

    edit doctor time seeder

--- database/seeders/DoctorTimeSeeder.php
+++ database/seeders/DoctorTimeSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use Carbon\CarbonPeriod;
 
 class DoctorTimeSeeder extends Seeder
 {
@@ -12,26 +13,29 @@ class DoctorTimeSeeder extends Seeder
     {
         $data = [];
 
-        $doctors = [1, 2, 3]; // IDs الدكاترة
-        $daysCount = 7; // من النهارده ولمدة 7 أيام
+        $doctors = [1, 2, 3, 197, 198, 199]; // IDs الدكاترة
+
+        $startDate = Carbon::today();               // من النهارده
+        $endDate   = Carbon::today()->addMonth();   // لشهر قدام
+
+        $period = CarbonPeriod::create($startDate, $endDate);
 
         foreach ($doctors as $doctorId) {
-            for ($i = 0; $i < $daysCount; $i++) {
-
-                // أي يوم من النهارده وما بعده
-                $date = Carbon::now()->addDays($i)->toDateString();
+            foreach ($period as $date) {
 
                 $start = Carbon::createFromTime(9, 0);
                 $end   = Carbon::createFromTime(15, 0);
 
                 while ($start->lt($end)) {
                     $slotEnd = $start->copy()->addHour();
+
                     $data[] = [
                         'doctor_id'  => $doctorId,
-                        'date'       => $date,
+                        'date'       => $date->toDateString(),
                         'start_time' => $start->format('H:i'),
                         'end_time'   => $slotEnd->format('H:i'),
                         'created_at' => now(),
+                        'updated_at' => now(),
                     ];
 
                     $start->addHour();
